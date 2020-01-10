48854f13601bd763f81ec3acc9b4ea63d245ab8a
Elhassnaoui Abdelouahab <57252825+E-Abdelouahab@users.noreply.github.com>
2020-01-10 10:25:25 +0100

    calculator Smart
    Smart calculator sans chiffres , utilisé html et css et js

---
+++ calcu/calcul/js/script.js
@@ -0,0 +1,30 @@
+
+
+function kolchi(){
+    var x = document.getElementById("input").value;
+    var a = x *1;
+    var y = document.getElementById("input1").value;
+    var z = y *1;
+    document.getElementById("res").innerHTML = a + z;
+}
+function kolchi1(){
+    var x = document.getElementById("input").value;
+    var a = x *1;
+    var y = document.getElementById("input1").value;
+    var z = y *1;
+    document.getElementById("res").innerHTML = a * z;
+}
+function kolchi2(){
+    var x = document.getElementById("input").value;
+    var a = x *1;
+    var y = document.getElementById("input1").value;
+    var z = y *1;
+    document.getElementById("res").innerHTML = a - z;
+}
+function kolchi3(){
+    var x = document.getElementById("input").value;
+    var a = x *1;
+    var y = document.getElementById("input1").value;
+    var z = y *1;
+    document.getElementById("res").innerHTML = a / z;
+}
